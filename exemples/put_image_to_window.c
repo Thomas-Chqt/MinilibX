a05ab49ea77a304b655eccf28f3a9dea773c1cf8
Thomas <semoir.dense-0h@icloud.com>
2024-06-09 23:49:19 +0900

    Update .gitignore and put_image_to_window.c

--- exemples/put_image_to_window.c
+++ exemples/put_image_to_window.c
@@ -29,16 +29,20 @@ int main()
     if ((data.window = mlx_new_window(data.mlx, 1280, 720, "Window exemple")) == NULL)
         return EXIT_FAILURE;
 
-    if ((data.img = mlx_new_image(data.mlx, 800, 600)) == NULL)
+    if ((data.img = mlx_new_image(data.mlx, 100, 100)) == NULL)
         return EXIT_FAILURE;
 
     int bits_per_pixel, size_line, endian;
     int* bytes = (int*)mlx_get_data_addr(data.img, &bits_per_pixel, &size_line, &endian);
 
-    for (int i = 0; i < 800 * 600; i++)
+    for (int i = 0; i < 100 * 100; i++)
         bytes[i] = 0x00ffffff;
 
     mlx_put_image_to_window(data.mlx, data.window, data.img, 0, 0);
+    mlx_put_image_to_window(data.mlx, data.window, data.img, 200, 0);
+    mlx_put_image_to_window(data.mlx, data.window, data.img, 0, 200);
+    mlx_put_image_to_window(data.mlx, data.window, data.img, 200, 200);
+    mlx_put_image_to_window(data.mlx, data.window, data.img, 400, 0);
 
     mlx_hook(data.window, 17, 0, (int(*)())exit, (void*)EXIT_SUCCESS);
 
